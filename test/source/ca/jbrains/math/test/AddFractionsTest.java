0e033931e2a89dcb8c670d815d3322d3d3343a4a
DIWithers <d.ivy.withers@gmail.com>
2016-12-18 15:25:35 -0500

    We now add non-zero, non-negative integers

--- test/source/ca/jbrains/math/test/AddFractionsTest.java
+++ test/source/ca/jbrains/math/test/AddFractionsTest.java
@@ -24,4 +24,9 @@ public class AddFractionsTest {
         final Fraction sum = new Fraction(0).plus(new Fraction(5));
         assertEquals(5, sum.intValue());
     }
+    @Test
+    public void nonNegativeNonZeroOperands() throws Exception {
+        final Fraction sum = new Fraction(3).plus(new Fraction(4));
+        assertEquals(7, sum.intValue());
+    }
 }
