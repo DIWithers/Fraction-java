e97c7df3f7475ba03d41e3754ba2358a0a0fa431
DIWithers <d.ivy.withers@gmail.com>
2016-12-18 14:41:30 -0500

    Add nonZero value test

--- production/source/ca/jbrains/math/Fraction.java
+++ production/source/ca/jbrains/math/Fraction.java
@@ -4,13 +4,16 @@ package ca.jbrains.math;
  * Created by daniellewithers on 12/18/16.
  */
 public class Fraction {
+    private final int integerValue;
+
     public Fraction(int integerValue) {
+        this.integerValue = integerValue;
 
     }
     public Fraction plus(Fraction that) {
         return this;
     }
     public int intValue() {
-        return 0;
+        return integerValue;
     }
 }
