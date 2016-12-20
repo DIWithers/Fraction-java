7e3a51f0f81907183ec769d7d5f6753a65ed2095
DIWithers <d.ivy.withers@gmail.com>
2016-12-20 11:21:22 -0500

    Added test for both 1 and non-1 denominators

--- test/source/ca/jbrains/math/test/AddFractionsTest.java
+++ test/source/ca/jbrains/math/test/AddFractionsTest.java
@@ -36,4 +36,12 @@ public class AddFractionsTest {
 //        Can also be written as:
 //        assertEquals(-2, new Fraction(-3).plus(new Fraction(1)).intValue());
     }
+    @Test
+    public void nonTrivialButCommonDenominator() throws Exception {
+        final Fraction sum = new Fraction(1, 5). plus(new Fraction(2, 5));
+        assertEquals(3, sum.getNumerator());
+        assertEquals(5, sum.getDenominator());
+
+    }
+
 }
